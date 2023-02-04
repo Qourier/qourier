21fdc70193649b5c88b04ff61d45defe26ebe242
Alex Baker <alex.baker.fon@gmail.com>
2023-02-04 18:17:53 +0200

    edit: new filecoin factory address

--- config/index.js
+++ config/index.js
@@ -21,7 +21,7 @@ export default {
     abi: f,
     contract: {
       localhost: "0x5FbDB2315678afecb367f032d93F642f64180aa3",
-      filecoin: "0xCF15c0eF3f8EdE35C2CFa1ca5395Ef5644A7eFD2",
+      filecoin: "0x50B72Baf990Ed475cfeC538b2fa525F00a8A4992",
     },
   },
   hub: {
